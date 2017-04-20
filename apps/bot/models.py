e6bf621f768839799a6b2a2a12961eb8c3296815
Igor Bakutin <igorbakutin@gmail.com>
2017-04-20 17:24:05 +0300

    Added logging for messages

--- apps/bot/models.py
+++ apps/bot/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.postgres.fields import JSONField
 from django.contrib.auth import get_user_model
 
+
 User = get_user_model()
 
 
@@ -9,6 +10,6 @@ class Message(models.Model):
     user = models.ForeignKey(User, related_name='messages')
     date = models.DateTimeField()
 
-    telegram_id = models.IntegerField(editable=False)
+    telegram_id = models.IntegerField(editable=False)  # unique per chat only
 
     raw = JSONField()
--- apps/bot/views.py
+++ apps/bot/views.py
@@ -1,13 +1,21 @@
 from django.http import HttpResponse
 from rest_framework.views import APIView
 
+from datetime import datetime
+
 from .auth import TelegramAuthentication
+from .models import Message
 
 
 class WebhookView(APIView):
     authentication_classes = [TelegramAuthentication]
 
     def post(self, request):
-        print(request.data)
-        print(request.user)
+        msg = Message.objects.create(
+            user=request.user,
+            raw=request.data['message'],
+            telegram_id=request.data['message']['message_id'],
+            date=datetime.fromtimestamp(request.data['message']['date']),
+        )
+        print(msg)
         return HttpResponse()
